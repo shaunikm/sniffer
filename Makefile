a665e96dbdf43139fb2a7380262829e104f84996
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2025-07-05 19:51:50 -0700

    init CMake proj

--- Makefile
+++
@@ -1,6 +0,0 @@
-.PHONY: build
-
-.DEFAULT: build
-
-build:
-	g++ main.cpp -o main -lpcap
--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
-#include <chrono>
 #include <iostream>
+#include <chrono>
+#include <stdlib.h>
 
 extern "C" {
-#include <pcap.h>
+    #include <pcap/pcap.h>
+}
+
+void throw_error(const char* msg) {
+    std::cerr << msg << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+void get_devices(pcap_if_t** devs, char* errbuf) {
+    if (pcap_findalldevs(devs, errbuf) == -1) {
+        std::cerr << "Interface not found:" << std::endl;
+        throw_error(errbuf);
+    }
 }
 
 int main() {
-  char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_if_t *all_devs;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_if_t *all_devs = nullptr;
+    pcap_t *handle = nullptr;
 
-  if (pcap_findalldevs(&all_devs, errbuf) == -1) {
-    std::cerr << "Couldn't find devices :(" << std::endl;
-    return 1;
-  }
+    get_devices(&all_devs, errbuf);
+    const char* dev = all_devs->name;
 
-  const char *dev = all_devs->name;
-  std::cout << "Device: " << dev << std::endl;
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == nullptr) {
+        std::cerr << "Couldn't open device:" << std::endl;
+        throw_error(errbuf);
+    }
 
-  pcap_freealldevs(all_devs);
+    std::cout << "Device: " << dev << std::endl;
+
+    pcap_freealldevs(all_devs);
 }
